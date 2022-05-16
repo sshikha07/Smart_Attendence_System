e3e51cece3e0ba010858d4ed07ea44f9b0a84016
Shikha Sharma <SSHIKHA7797@gmail.com>
2022-05-16 20:47:52 +0530

    Code added

---
+++ Code/automail.py
@@ -0,0 +1,17 @@
+import yagmail
+import cv2
+import os
+
+receiver = "@gmail.com"  # receiver email address
+body = "Attendence File"  # email body
+filename = "Attendance"+os.sep+"Attendance_2020-08-29_17-05-04.csv" 
+
+yag = yagmail.SMTP("mymail@gmail.com", "my_password")
+
+yag.send(
+    to=receiver,
+    subject="Attendance Report", 
+    contents=body, 
+    attachments=filename, 
+)
+
